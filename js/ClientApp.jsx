916d246fcb6ac4c0b7d1617273a6392290a218a5
Priscila Oliveira <Priscila.Oliveira@ataccama.com>
2017-06-07 21:19:08 +0200

    fix(FEM-03): Fixed Webpack dependencies problems and removed MyTitle

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MyTitle from './MyTitle'
+import { render } from 'react-dom'
+import '../public/normalize.css'
+import '../public/style.css'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
-var MyFirstComponent = React.createClass({
-  render: function () {
+const App = React.createClass({
+  render () {
     return (
-      div(null,
-        MyTitleFactory({ title: 'props are the best', color: 'peru' }),
-        MyTitleFactory({ title: 'semicolons are the worst', color: 'mediumaquamarine' }),
-        MyTitleFactory({ title: 'jklol its okay if you like semicolons', color: 'rebeccapurple' }),
-        MyTitleFactory({ title: 'im out of ideas', color: 'papayawhip' })
-      )
+      <div className='app'>
+        <div className='landing'>
+          <h1>svideo</h1>
+          <input type='text' placeholder='Search' />
+          <a>or Browse All</a>
+        </div>
+      </div>
     )
   }
 })
 
-ReactDOM.render(
-  React.createElement(MyFirstComponent),
-  document.getElementById('app')
-)
+render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,56 @@ const path = require('path')
 
 module.exports = {
   context: __dirname,
-  entry: './js/ClientApp.jsx',
+  entry: './js/ClientApp',
   devtool: 'eval',
   output: {
     path: path.join(__dirname, '/public'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
+  },
+  devServer: {
     publicPath: '/public/'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['.js', '.json', '.jsx']
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: true
+    chunks: true,
+    errorDetails: true
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader'
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.json$/,
         loader: 'json-loader'
+      },
+      {
+        include: path.resolve(__dirname, 'js'),
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        query:
+        {
+          presets:['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
       }
     ]
   }
